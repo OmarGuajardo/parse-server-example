aec572bea6e8adbb294aea6224ceaf348b7cd6f6
OmarGuajardo <omar.guajardog@outlook.com>
2020-07-24 15:37:59 -0700

    test func

--- cloud/main.js
+++ cloud/main.js
@@ -1,3 +1,13 @@
+//CLOUD FUNCS
+
+//TODO: get the key from .env
+Parse.initialize(process.env.APP_ID);
+Parse.serverURL = 'http://localhost:1337/parse'
+
 Parse.Cloud.define('hello', function(req, res) {
   return 'Hi';
 });
+
+Parse.Cloud.define("hello_world_omar",function(){
+  return 'Hello from FBU at Texas';
+})
