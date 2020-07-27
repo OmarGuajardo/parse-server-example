2f61cceebbc5f3f3383a8090fece8ac1eee1936d
OmarGuajardo <omar.guajardog@outlook.com>
2020-07-27 08:53:17 -0700

    updateList funciton complete

--- cloud/main.js
+++ cloud/main.js
@@ -1,14 +1,29 @@
 //CLOUD FUNCS
 
-// //TODO: get the key from .env
-// Parse.initialize(process.env.APP_ID);
-// Parse.serverURL = 'http://localhost:1337/parse'
-let x = 2;
+let listLayer = [];
+let listTop = [];
+let listBottom = [];
+let listShoes = [];
+
+
 Parse.Cloud.define('hello', function(req, res) {
-  return x;
+  return "Hello new function " + name;
 });
-Parse.Cloud.define('get_user_name', function(objID, res) {
-  return Parse.User.get(objID).get("username");
+
+Parse.Cloud.define("updateLists", async (request)=>{
+  var query = new Parse.Query("Closet");
+  var userCloset = query.get(request.params.currentUserCloset);
+  let layerRelation = (await userCloset).relation("Layer");
+  let topRelation = (await userCloset).relation("Top");
+  let bottomRelation = (await userCloset).relation("Bottom");
+  let shoesRelation = (await userCloset).relation("Shoes");
+
+
+  listLayer = (await layerRelation.query().find());
+  listTop = (await topRelation.query().find());
+  listBottom = (await bottomRelation.query().find());
+  listShoes = (await shoesRelation.query().find());
+  return ("Lists updated!");
 });
 
 
